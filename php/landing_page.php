4fcf96446fede8e0530d6b30acdb4989d376f780
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-11-24 14:43:55 -0800

    Added RegisterAccount.php, added forms. added CreateAccount.php added Stylesheet.css

--- php/landing_page.php
+++ php/landing_page.php
@@ -36,7 +36,7 @@ if (filter_input (INPUT_COOKIE, 'auth') == session_id()) {
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="../index.html">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="#tracks">Courses</a></li>
                 <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
             </ul>
