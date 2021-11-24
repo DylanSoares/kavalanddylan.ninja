3c1d667e9781024f44df369f8cab8e3a55913324
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-11-24 11:07:22 -0800

    Added RegisterAccount.php, added forms. added CreateAccount.php added Stylesheet.css

--- js/validation.js
+++ js/validation.js
@@ -1,7 +1,23 @@
 function validateEmail() {
     let text;
-
     text = "Email alaredy exists";
     document.getElementById("emailText").innerHTML = text;
 }
 
+function validateForm() {
+    let php = "<?php $mysqli = mysqli_connect(\"localhost\", \"cs213user\", \"letmein\", \"RALLYCO\");" +
+        "$email = strtolower($_POST[\"email\"]);" +
+        "$sql = \"SELECT email FROM Drivers WHERE email = '\" . $email .\n" +
+        "\"'\";" +
+        "if (mysqli_num_rows($result) >= 1) {" +
+        "echo \"<script type=\"text/javascript\">validateForm(false)</script>\";}?>";
+    console.log(php);
+}
+
+function validateForm(status) {
+    if (status === false) {
+        return false;
+    }else{
+        return true;
+    }
+}
