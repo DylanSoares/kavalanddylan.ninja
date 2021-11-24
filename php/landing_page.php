7d5878a24369b91d43b31ea96df87fb1448b3d0a
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-11-24 15:51:07 -0800

    Added RegisterAccount.php, added forms. added CreateAccount.php added Stylesheet.css

--- php/landing_page.php
+++ php/landing_page.php
@@ -49,8 +49,8 @@ if (filter_input (INPUT_COOKIE, 'auth') == session_id()) {
     <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
         <div class="d-flex justify-content-center">
             <div class="text-center">
-                <h1 class="mx-auto my-0 text-uppercase">Rally Co</h1>
-                <h2 class="text-white-50 mx-auto mt-2 mb-5">Welcome! Click a course to get started.</h2>
+                <h1 class="mx-auto my-0 text-uppercase">Welcome</h1>
+                <h2 class="text-white-50 mx-auto mt-2 mb-5">Click a course to get randomly generated course notes.</h2>
                 <!--<a class="btn btn-primary" href="Login.html">Login</a>
                 <a class="btn btn-primary" href="RegisterAccount.php">Register</a>-->
             </div>
