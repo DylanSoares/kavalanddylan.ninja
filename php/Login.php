9e11853cf14bbf60b61530781941a82cb7f3d213
KavalS <kavaljeetsingh95@outlook.com>
2021-11-24 12:01:22 -0800

    login page

--- php/Login.php
+++ php/Login.php
@@ -15,9 +15,10 @@
     <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="../css/Stylesheet.css" rel="stylesheet">
+    <link href="../css/Stylesheet.css" rel="stylesheet" />
 </head>
 <body id="page-top">
+<h2>login test</h2>
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container px-4 px-lg-5">
