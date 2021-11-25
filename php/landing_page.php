f7a6ccff8e78f0e4f94ff49b1aef8604f9c3805c
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-11-24 16:12:49 -0800

    Added RegisterAccount.php, added forms. added CreateAccount.php added Stylesheet.css

--- php/landing_page.php
+++ php/landing_page.php
@@ -64,9 +64,11 @@ if (filter_input (INPUT_COOKIE, 'auth') == session_id()) {
         <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
             <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="../assets/img/Coscto_Cup.jpg" alt="..." /></div>
             <div class="col-xl-4 col-lg-5">
-                <div class="featured-text text-center text-lg-left">
+                <div class="featured-text bg-mid text-center text-lg-left">
+                    <br>
                     <h4 class="text-white">Costco Cup</h4>
                     <p class="text-white-50 mb-0">Costco Cup is a prestigious rally event attended by many. Though some say the chaos doesn't end even after you arrive.</p>
+                    <br>
                 </div>
             </div>
         </div>
