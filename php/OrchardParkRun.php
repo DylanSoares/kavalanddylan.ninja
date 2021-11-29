9c2cc31e56b345002a43dd75f9d98f9cf77512ae
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-11-29 12:02:18 -0800

    Updated

--- php/OrchardParkRun.php
+++ php/OrchardParkRun.php
@@ -1,27 +1,29 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
     <title>Kaval & Dylan's Rally Co</title>
-    <link rel="icon" type="image/x-icon" href="../assets/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="../assets/favicon.ico"/>
     <!-- Font Awesome icons (free version)-->
     <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
     <!-- Google fonts-->
-    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+          rel="stylesheet"/>
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="../css/Stylesheet.css" rel="stylesheet" />
+    <link href="../css/Stylesheet.css" rel="stylesheet"/>
 </head>
 <body id="page-top">
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container px-4 px-lg-5">
         <a class="navbar-brand" href="#page-top">Kaval & Dylan Rally Co</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars"></i>
         </button>
@@ -34,16 +36,7 @@
     </div>
 </nav>
 <!-- Masthead-->
-<header id="welcome" class="masthead">
-    <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
-        <div class="d-flex justify-content-center">
-            <div class="text-center">
-                <h1 class="mx-auto my-0 text-uppercase">Course Notes</h1>
-                <h2 class="text-white-50 mx-auto mt-2 mb-5">Welcome to Rally Co!<br><br>Select a course below to get randomly generated course notes.</h2>
-            </div>
-        </div>
-    </div>
-</header>
+
 <!-- -->
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
@@ -52,6 +45,9 @@ $mysqli = mysqli_connect("localhost", "cosc219", "letmein", "Drivers");
 //echo "<h2>Members Table:</h2><br>";
 $sql = "SELECT * FROM members_table";
 $result = $mysqli->query($sql);
+echo "<header id=\"welcome\" class=\"masthead\">
+    <div class=\"container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center\">
+        <div class=\"d-flex justify-content-center\">";
 
 echo "<table>";
 //echo table header for the first row
@@ -62,6 +58,8 @@ while ($row = $result->fetch_assoc()) {
 }
 
 echo "</table><br>";
+echo "</div></div></header>";
+
 ?>
 </body>
 <footer class="footer bg-black small text-center text-white-50">
