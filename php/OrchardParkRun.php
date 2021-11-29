56fbf3ccb49dc74709caf0dfb506a8a91d173f18
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-11-29 14:42:32 -0800

    Updated

--- php/OrchardParkRun.php
+++ php/OrchardParkRun.php
@@ -53,6 +53,7 @@
     <div>
         <a id="generate" class="btn btn-primary"
            onclick="$.getScript('../js/GenerateAgain.js', function() {onClick();});">Generate Again</a>
+        <a class="btn btn-primary" href="LandingPage.php#tracks">Select Track</a>
     </div>
     <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
         <div class="d-flex justify-content-center">
