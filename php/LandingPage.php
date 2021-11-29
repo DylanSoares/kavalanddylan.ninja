0a826a874f3024b521dfd316a684a64c75145d46
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-11-29 12:46:48 -0800

    Updated

--- php/LandingPage.php
+++ php/LandingPage.php
@@ -83,15 +83,18 @@ if (filter_input (INPUT_COOKIE, 'auth') == session_id()) {
         </a>
         <!-- Project One Row-->
         <!--TODO KFC Sprint Query-->
-        <a class="course" href="KFCSprint.php">
-            <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
-                <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="../assets/img/KFC_Sprint.jpg" alt="..." /></div>
-                <div class="col-xl-4 col-lg-5">
-                    <div class="featured-text bg-mid text-center text-lg-left">
-                        <br>
-                        <h4 class="text-white">KFC Sprint</h4>
-                        <p class="text-white-50 mb-0">The KFC Sprint is a college student's main event, often done with limited time. A true showcase in parking lot maneuvering.</p>
-                        <br>
+        <a class="course" href="KFCSprint.php.php">
+            <div class="row gx-0 justify-content-center">
+                <div class="col-lg-6"><img class="img-fluid" src="../assets/img/KFC_Sprint.jpg" alt="..." /></div>
+                <div class="col-lg-6 order-lg-first">
+                    <div class="bg-mid text-center h-100 project">
+                        <div class="d-flex h-100">
+                            <div class="project-text w-100 my-auto text-center text-lg-right">
+                                <h4 class="text-white">KFC Sprint</h4>
+                                <p class="text-white-50 mb-0">The KFC Sprint is a college student's main event, often done with limited time. A true showcase in parking lot maneuvering.</p>
+                                <hr class="d-none d-lg-block mb-0 me-0" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
