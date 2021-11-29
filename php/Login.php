d6589325ec798bb6efcc1c0465cdac7d3f01b1ff
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-11-29 11:24:35 -0800

    Updated

--- php/Login.php
+++ php/Login.php
@@ -1,8 +1,30 @@
 <?php
-//login php to handle the login,
-//if successful should go to landing_page.html
-//if not redirect
+//login php to handle the login
 
-/*
-header("Location: ./Login.php");
-exit();*/
+//connect to server and select database
+$mysqli = mysqli_connect("localhost", "cs213user", "letmein", "RALLYCO");
+//create and issue the query
+$targetname = filter_input(INPUT_POST, 'email');
+$targetpasswd = filter_input(INPUT_POST, 'password');
+$sql = "SELECT email, fname, lname, team, car FROM Drivers WHERE email = '" . strtolower($targetname) .
+    "' AND pword = SHA1('" . $targetpasswd . "')";
+$result = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
+//get the number of rows in the result set; should be 1 if a match
+if (mysqli_num_rows($result) == 1) {
+    //if authorized, get the values of f_name l_name
+    while ($info = mysqli_fetch_array($result)) {
+        $f_name = stripslashes($info['firstname']);
+        $l_name = stripslashes($info['lastname']);
+        $email = stripslashes($info['email']);
+        $team = stripslashes($info['team']);
+        $car = stripslashes($info['car']);
+    }
+    //set authorization cookie using current Session ID
+    setcookie("auth", session_id(), time() + 60 * 30, "/", "", 0);
+    header("Location: landing_page.php");
+    //create display string
+} else {
+    //redirect back to login form if not authorized
+    header("Location: ../Login.html");
+}
+exit;
