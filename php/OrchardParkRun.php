4488a788e55697d757f12215b74de2b5bcd3ecde
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-11-29 14:33:18 -0800

    Updated

--- php/OrchardParkRun.php
+++ php/OrchardParkRun.php
@@ -104,9 +104,7 @@
                 ?>
                 </tbody>
             </table>
-            <a id="generate" class="btn btn-primary" onclick="$.getScript('external.js', function() {
-            addClick();
-        });">Generate Again</a>
+            <a id="generate" class="btn btn-primary" onclick="$.getScript('../js/GenerateAgain.js', function() {addClick();});">Generate Again</a>
         </div>
     </div>
 </section>
