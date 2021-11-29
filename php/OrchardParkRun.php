692b3905ed50aa02b9d5b50a6c1c66b912c0da4d
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-11-29 15:14:45 -0800

    update

--- php/OrchardParkRun.php
+++ php/OrchardParkRun.php
@@ -69,45 +69,8 @@
                 </thead>
                 <tbody>
                 <?php
-                $i = 0;
-                $j = 0;
-                $_leftArr[0] = "";
-                $_rightArr[0] = "";
-                mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-                $mysqli = mysqli_connect("localhost", "cs213user", "letmein", "RALLYCO");
-
-                $sql = "SELECT * FROM Orchard_Park_Run";
-                $result = $mysqli->query($sql);
-
-                $leftTurns = "SELECT turnQuote FROM LeftTurns";
-                $leftResult = $mysqli->query($leftTurns);
-                while ($row = $leftResult->fetch_assoc()) {
-                    $_leftArr[$i] = $row["turnQuote"];
-                    $i++;
-                }
-
-                $rightTurns = "SELECT turnQuote FROM RightTurns";
-                $rightResult = $mysqli->query($rightTurns);
-                while ($row = $rightResult->fetch_assoc()) {
-                    $_rightArr[$j] = $row["turnQuote"];
-                    $j++;
-                }
-
-                //echo table rows for each row returned
-                while ($row = $result->fetch_assoc()) {
-                    echo "<tr><td>" . $row["Turn_ID"] . "</td><td>" . $row["Turn_Dir"] . "</td><td>";
-                    switch ($row["Turn_Dir"]) {
-                        case "left":
-                            $leftQuote = mt_rand(0, sizeof($_leftArr) - 1);
-                            echo $_leftArr[$leftQuote];
-                            break;
-                        case "right":
-                            $rightQuote = mt_rand(0, sizeof($_rightArr) - 1);
-                            echo $_rightArr[$rightQuote];
-                            break;
-                    }
-                    echo "</td></tr>";
-                }
+                include('TableGenerator.php');
+                echo GenerateTable("Orchard_Park_Run");
                 ?>
                 </tbody>
             </table>
