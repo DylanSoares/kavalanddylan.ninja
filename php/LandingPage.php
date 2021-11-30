9428c26dc60245d4b67d3cb65433711f778f57f8
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-11-29 17:30:10 -0800

    update

--- php/LandingPage.php
+++ php/LandingPage.php
@@ -176,6 +176,7 @@ if (filter_input(INPUT_COOKIE, 'auth') != session_id()) {
 <!-- Footer-->
 <footer class="footer bg-black small text-center text-white-50">
     <div class="container px-4 px-lg-5">Totally Legit Copyright &copy; Kaval & Dylan 2021</div>
+    <div class="container px-4 px-lg-5">This entire thing is an elaborate inside joke.</div>
 </footer>
 <!-- Bootstrap core JS-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
