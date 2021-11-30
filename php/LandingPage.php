5e19909bf0f3dad675bd57b1c7166eac2d9306c9
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-11-29 17:13:17 -0800

    update

--- php/LandingPage.php
+++ php/LandingPage.php
@@ -9,26 +9,29 @@ if (filter_input(INPUT_COOKIE, 'auth') != session_id()) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
     <title>Kaval & Dylan's Rally Co</title>
-    <link rel="icon" type="image/x-icon" href="../assets/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="../assets/favicon.ico"/>
     <!-- Font Awesome icons (free version)-->
     <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
     <!-- Google fonts-->
-    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+          rel="stylesheet"/>
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="../css/Stylesheet.css" rel="stylesheet" />
+    <link href="../css/Stylesheet.css" rel="stylesheet"/>
 </head>
 <body id="page-top">
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container px-4 px-lg-5">
         <a class="navbar-brand" href="#page-top">Kaval & Dylan Rally Co</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars"></i>
         </button>
@@ -48,7 +51,8 @@ if (filter_input(INPUT_COOKIE, 'auth') != session_id()) {
         <div class="d-flex justify-content-center">
             <div class="text-center">
                 <h1 class="mx-auto my-0 text-uppercase">Course Notes</h1>
-                <h2 class="text-white-50 mx-auto mt-2 mb-5">Welcome to Rally Co!<br><br>Select a course below to get randomly generated course notes.</h2>
+                <h2 class="text-white-50 mx-auto mt-2 mb-5">Welcome to Rally Co!<br><br>Select a course below to get
+                    randomly generated course notes.</h2>
             </div>
         </div>
     </div>
@@ -66,7 +70,8 @@ if (filter_input(INPUT_COOKIE, 'auth') != session_id()) {
         <!-- Featured Project Row-->
         <a class="course" href="CostcoCup.php">
             <div class="row gx-0 mb-4 mb-lg-4 align-items-center">
-                <div class="col-xl-8 col-lg-7 orange-border"><img class="img-fluid mb-lg-0" src="../assets/img/Costco_Cup_DARK_COLOR.jpg"
+                <div class="col-xl-8 col-lg-7 orange-border"><img class="img-fluid mb-lg-0"
+                                                                  src="../assets/img/Costco_Cup_DARK_COLOR.jpg"
                                                                   alt="..."/></div>
                 <div class="col-xl-4 col-lg-5">
                     <div class="featured-text bg-mid text-center text-lg-left">
@@ -82,7 +87,8 @@ if (filter_input(INPUT_COOKIE, 'auth') != session_id()) {
         <!-- Project One Row-->
         <a class="course" href="KFCSprint.php">
             <div class="row gx-0 justify-content-center">
-                <div class="col-lg-6 orange-border"><img class="img-fluid" src="../assets/img/KFC_Sprint_DARK_COLOR.jpg" alt="..."/></div>
+                <div class="col-lg-6 orange-border"><img class="img-fluid" src="../assets/img/KFC_Sprint_DARK_COLOR.jpg"
+                                                         alt="..."/></div>
                 <div class="col-lg-6 order-lg-first">
                     <div class="bg-mid text-center h-100 project">
                         <div class="d-flex h-100">
@@ -100,7 +106,9 @@ if (filter_input(INPUT_COOKIE, 'auth') != session_id()) {
         <!-- Project Two Row-->
         <a class="course" href="OrchardParkRun.php">
             <div class="row gx-0 pb-5 mt-4 justify-content-center">
-                <div class="col-lg-6 orange-border"><img class="img-fluid" src="../assets/img/Orchard_Park_Run_DARK_COLOR.jpg" alt="..."/></div>
+                <div class="col-lg-6 orange-border"><img class="img-fluid"
+                                                         src="../assets/img/Orchard_Park_Run_DARK_COLOR.jpg" alt="..."/>
+                </div>
                 <div class="col-lg-6 order-lg-first">
                     <div class="bg-mid text-center h-100 project">
                         <div class="d-flex h-100">
