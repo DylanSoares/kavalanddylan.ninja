eadd0481d432fd77c996429ef86586633815f7b6
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-11-29 17:19:33 -0800

    update

--- php/LandingPage.php
+++ php/LandingPage.php
@@ -5,7 +5,6 @@ if (filter_input(INPUT_COOKIE, 'auth') != session_id()) {
     exit;
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -181,6 +180,7 @@ if (filter_input(INPUT_COOKIE, 'auth') != session_id()) {
 <!-- Bootstrap core JS-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Core theme JS-->
-<script src="js/scripts.js"></script>
+<script src="../js/scripts.js"></script>
 </body>
 </html>
+
