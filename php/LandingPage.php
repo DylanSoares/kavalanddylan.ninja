656c62b1ff0ec790f9664e44ff305b823ea2846d
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-12-01 11:33:13 -0800

    Updated

--- php/LandingPage.php
+++ php/LandingPage.php
@@ -41,6 +41,8 @@ if (!isset($_SESSION['id']) ||(trim ($_SESSION['id']) == '')) {
                 <li class="nav-item"><a class="nav-link" href="#tracks">Courses</a></li>
                 <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
                 <li class="nav-item"><a class="nav-link" href="Logout.php">Log Out</a></li>
+                <li class="dropdown-menu">Profile</li>
+
             </ul>
         </div>
     </div>
