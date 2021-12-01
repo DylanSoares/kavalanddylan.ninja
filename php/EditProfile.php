daae0f3ef06879b9a8e74c72861d643b9693a9eb
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-12-01 12:40:35 -0800

    Updated

--- php/EditProfile.php
+++ php/EditProfile.php
@@ -71,6 +71,51 @@ if (!isset($_SESSION['id']) || (trim($_SESSION['id']) == '')) {
                 <h3>Personal info</h3>
 
                 <form class="form-signup" role="form">
+                    <div class="row input-group-newsletter">
+                        <div class="col">
+                            <label class="col-form-label" for="team">Select Team</label>
+                            <select class="form-control" name="team" id="team" required>
+                                <option value="subaru_motorsports">Subaru Motorsports</option>
+                                <option value="mitsubishi_ralliart">Mitsubishi Ralliart</option>
+                                <option value="toyota_gazoo_racing">Toyota Gazoo Racing</option>
+                                <option value="ford_world_rally_team">Ford World Rally Team</option>
+                                <option value="hyundai_motorsport">Hyundai Motorsport</option>
+                                <option value="citroen_world_rally_team">Citroën World Rally Team</option>
+                            </select>
+                        </div>
+                        <div class="col-auto">
+                            <label class="col-form-label" for="car">Select Car</label>
+                            <select class="form-control" name="car" id="car" required>
+                                <optgroup label="Subaru Cars">
+                                    <option value="subaru_s8_wrc">Subaru S8 WRC</option>
+                                    <option value="subaru_impreza_wrc">Subaru Impreza WRC</option>
+                                    <option value="subaru_wrx_sti">Subaru WRX STI</option>
+                                </optgroup>
+                                <optgroup label="Mitsubishi Cars">
+                                    <option value="mitsubishi_lancer_wrc">Mitsubishi Lancer WRC</option>
+                                    <option value="mitsubishi_lancer_evo">Mitsubishi Lancer EVO</option>
+                                    <option value="mitsubishi_xpander_ap4">Mitsubishi XPANDER AP4</option>
+                                </optgroup>
+                                <optgroup label="Toyota Cars">
+                                    <option value="toyota_yaris_wrc">Toyota Yaris WRC</option>
+                                    <option value="toyota_yaris_rally1">Toyota GR Yaris Rally1</option>
+                                </optgroup>
+                                <optgroup label="Ford Cars">
+                                    <option value="ford_fiesta_wrc">Ford Fiesta WRC</option>
+                                    <option value="ford_fiesta_rs_wrc">Ford Fiesta RS WRC</option>
+                                    <option value="ford_puma_rally1">Ford Puma Rally1</option>
+                                </optgroup>
+                                <optgroup label="Hyundai Cars">
+                                    <option value="hyundai_i20_coupe_wrc">Hyundai i20 Coupe WRC</option>
+                                    <option value="hyundai_i20_n_rally1">Hyundai i20 N Rally1</option>
+                                </optgroup>
+                                <optgroup label="Citroën Cars">
+                                    <option value="citroen_c3_wrc">Citroën C3 WRC</option>
+                                    <option value="citroen_ds3_wrc">Citroën DS3 WRC</option>
+                                </optgroup>
+                            </select>
+                        </div>
+                    </div>
                     <div class="form-group">
                         <label class="col-lg-3 control-label">First name:</label>
                         <div class="col-lg-8">
