2d9427314870dbc1348bcf3fa971aa717c451628
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-12-01 09:02:15 -0800

    Updated

--- php/TableGenerator.php
+++ php/TableGenerator.php
@@ -43,15 +43,13 @@ function GenerateTable($track, $email): string
     }
 
     $sql1 = "SELECT email FROM TrackRegistration WHERE email = '" . strtolower($email) . "'";
-    echo $sql1;
     $result = $mysqli->query($sql1);
 
     if (mysqli_num_rows($result) != 1) {
-        echo "Entered";
         $sql2 = "INSERT INTO TrackRegistration (email, KFC_Sprint, Costco_Cup, Orchard_Park_Run) VALUES ('" . $email . "', 0, 0, 0)";
         $mysqli->query($sql2);
     }
-    $sql3 = "UPDATE TrackRegistration SET " . $track . "= 1 WHERE email =" . $email;
+    $sql3 = "UPDATE TrackRegistration SET " . $track . "= 1 WHERE email ='" . $email . "'";
     $mysqli->query($sql3);
 
     return $output;
