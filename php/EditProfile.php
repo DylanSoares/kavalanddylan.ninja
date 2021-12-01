46c44d620bf47b830f6ecd9886800cdb2426767d
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-12-01 14:03:49 -0800

    Updated

--- php/EditProfile.php
+++ php/EditProfile.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-getFname($_SESSION['id']);
+include("FillProfileFields.php");
 
 if (!isset($_SESSION['id']) || (trim($_SESSION['id']) == '')) {
     header('Location: LoginPage.php');
