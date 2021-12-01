1a7788d2350f38c671de7629d03726414d2799b9
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-12-01 12:18:54 -0800

    Updated

--- php/EditProfile.php
+++ php/EditProfile.php
@@ -55,8 +55,7 @@ if (!isset($_SESSION['id']) || (trim($_SESSION['id']) == '')) {
     </div>
 </nav>
 <!-- Masthead-->
-<!-- Masthead-->
-<header id="masthead" class="masthead">
+<header id="masthead">
     <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
         <div class="d-flex justify-content-center">
             <div class="text-center">
