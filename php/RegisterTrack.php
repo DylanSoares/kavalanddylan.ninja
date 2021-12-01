c049f6b198aa2002728c7ae77040f76868b88a0a
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-12-01 08:48:10 -0800

    Updated

--- php/RegisterTrack.php
+++ php/RegisterTrack.php
@@ -4,11 +4,11 @@ function TrackRegistration($track, $email)
     include('DBConnect.php');
     $mysqli = connectToDB();
 
-    $sql1 = "SELECT email FROM TrackRegistration WHERE email =".$email.";";
+    $sql1 = "SELECT email FROM TrackRegistration WHERE email =\"".$email."\"";
     $result = $mysqli->query($sql1);
     if (mysqli_num_rows($result) != 1) {
         echo "Entered";
-        $sql2 = "INSERT INTO TrackRegistration (email, KFC_Sprint, Costco_Cup, Orchard_Park_Run) VALUES (" . $email . ", 0, 0, 0)";
+        $sql2 = "INSERT INTO TrackRegistration (email, KFC_Sprint, Costco_Cup, Orchard_Park_Run) VALUES (\"" . $email . "\", 0, 0, 0)";
         $mysqli->query($sql2);
     }
     $sql3 = "UPDATE TrackRegistration SET " . $track . "= 1 WHERE email =" . $email;
