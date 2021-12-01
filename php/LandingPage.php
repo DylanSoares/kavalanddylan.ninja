2069d1dba004969e7d56610b0be3cbaa48bf9686
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-12-01 11:40:56 -0800

    Updated

--- php/LandingPage.php
+++ php/LandingPage.php
@@ -41,8 +41,17 @@ if (!isset($_SESSION['id']) ||(trim ($_SESSION['id']) == '')) {
                 <li class="nav-item"><a class="nav-link" href="#tracks">Courses</a></li>
                 <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
                 <li class="nav-item"><a class="nav-link" href="Logout.php">Log Out</a></li>
-                <li class="dropdown-menu-start nav-item">Profile</li>
-
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
             </ul>
         </div>
     </div>
