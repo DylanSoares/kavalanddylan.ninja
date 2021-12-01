891fd5fd73370d8dad228bd9abf69c4fa5fcefd3
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-12-01 09:36:46 -0800

    Updated

--- php/JoinTableGenerator.php
+++ php/JoinTableGenerator.php
@@ -10,7 +10,7 @@ function JoinGenerateTable(): string
 
     //echo table rows for each row returned
     while ($row = $result->fetch_assoc()) {
-        $output = $output . "<tr><td>" . $row["d.fname"] . "</td><td>" . $row["t.KFC_Sprint"] . "</td><td>" . $row["t.Costco_Cup"] . "</td><td>" . $row["t.Orchard_Park_Run"] . "</td></tr>>";
+        $output = $output . "<tr><td>" . $row["d.fname"] . "</td><td>" . $row["t.KFC_Sprint"] . "</td><td>" . $row["t.Costco_Cup"] . "</td><td>" . $row["t.Orchard_Park_Run"] . "</td></tr>";
     }
 
     return $output;
