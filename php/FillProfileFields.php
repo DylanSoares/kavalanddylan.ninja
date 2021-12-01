82fdcc144afbfa80c8c0d1ba8ec9c938660efa53
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-12-01 14:22:00 -0800

    Updated

--- php/FillProfileFields.php
+++ php/FillProfileFields.php
@@ -8,7 +8,7 @@ function getFname($email): string
     echo "Line3";
     $mysqli = connectToDB();
     echo "Line4";
-    $result = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
+    $result = $mysqli->query($sql);
     echo "Line5";
     echo $result;
     echo $result['fname'];
