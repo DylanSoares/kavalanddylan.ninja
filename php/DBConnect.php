cb161c296f384769d2987625fe0a1cc760951858
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-12-01 08:53:52 -0800

    Updated

--- php/DBConnect.php
+++ php/DBConnect.php
@@ -1,5 +1,11 @@
 <?php
-function connectToDB() : mysqli {
+function connectToDB(): mysqli
+{
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
     return mysqli_connect("localhost", "cs213user", "letmein", "RALLYCO");
 }
