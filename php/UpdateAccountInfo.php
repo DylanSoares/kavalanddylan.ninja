b03ac70b80becc647eacb08139a9e51e24e1c48e
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-12-01 17:11:00 -0800

    Updated

--- php/UpdateAccountInfo.php
+++ php/UpdateAccountInfo.php
@@ -5,10 +5,6 @@ if (!isset($_SESSION['id']) || (trim($_SESSION['id']) == '')) {
     exit();
 }
 
-$target_dir = "../uploaddir/" . $_SESSION['id'] . "/";
-$target_file = $target_dir . "avatar.png";
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 /*
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $mysqli = mysqli_connect("localhost", "cs213user", "letmein", "RALLYCO");
@@ -101,45 +97,51 @@ if (file_exists($target_file)) {
     unlink($target_file); //remove the file
 }*/
 
-if (isset($_POST["submitButton"])) {
+$target_dir = "../uploaddir/".$_SESSION['id']."/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submitButton"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if ($check !== false) {
-        //echo "File is an image - " . $check["mime"] . ".";
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
-        //echo "File is not an image.";
+        echo "File is not an image.";
         $uploadOk = 0;
     }
 }
 
 // Check if file already exists
 if (file_exists($target_file)) {
-    //echo "Sorry, file already exists.";
+    echo "Sorry, file already exists.";
     $uploadOk = 0;
 }
 
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-    //echo "Sorry, your file is too large.";
+    echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
 
 // Allow certain file formats
-if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif") {
-    //echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    //echo "Sorry, your file was not uploaded.";
+    echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        //echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+        echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
     } else {
-        //echo "Sorry, there was an error uploading your file.";
+        echo "Sorry, there was an error uploading your file.";
     }
 }
 header('Location: EditProfile.php');
