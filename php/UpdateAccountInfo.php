8cc1189f97cf507fa91e9e822baac3f53b3f7c67
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-12-01 19:48:09 -0800

    Updated

--- php/UpdateAccountInfo.php
+++ php/UpdateAccountInfo.php
@@ -108,36 +108,35 @@ if (isset($_POST['submitButton'])) {
     if(isset($_POST["submit"])) {
         $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
         if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
             $uploadOk = 1;
         } else {
-            echo "File is not an image.";
+            echo "<script> alert(\"Error: File is not an image.\");</script>";
             $uploadOk = 0;
         }
     }
 
 // Check file size
     if ($_FILES["fileToUpload"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
+        echo "<script> alert(\"Sorry, your file is too large.\");</script>";
         $uploadOk = 0;
     }
 
 // Allow certain file formats
     if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
         && $imageFileType != "gif" ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        echo "<script> alert(\"Sorry, only JPG, JPEG, PNG & GIF files are allowed.\");</script>";
         $uploadOk = 0;
     }
 
 // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
+        echo "<script> alert(\"File upload error.\");</script>";
 // if everything is ok, try to upload file
     } else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+            echo "<script> alert(\"Account succesfully updated.\");</script>";
         } else {
-            echo "Sorry, there was an error uploading your file.";
+            echo "<script> alert(\"Error uploading file.\");</script>";
         }
     }
 
