a9be0860ae13534919f0c98a41e29fb1ba284e6f
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-12-01 16:33:14 -0800

    Updated

--- php/UpdateAccountInfo.php
+++ php/UpdateAccountInfo.php
@@ -92,17 +92,18 @@ if (isset($_POST['team'])) {
 $sql = "UPDATE Drivers SET fname = \"" . $firstname . "\", lname = \"" . $lastname . "\", team =\"" . $teamSelected . "\", car = \"" . $carSelected . "\" WHERE email = \"" . $_SESSION['id'] . "\"";
 $mysqli->query($sql);
 
-// Check if image file is a actual image or fake image
+if (isset($_POST["submit"])) {
+    // Check if image file is a actual image or fake image
 
-$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-if ($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-} else {
-    echo "File is not an image.";
-    $uploadOk = 0;
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if ($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
 }
-
 
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
@@ -110,7 +111,7 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
 }
 //check if file exists
 if (file_exists($target_file)) {
-    chmod($target_file, 0755); //Change the file permissions if allowed
+    chmod($target_file,0755); //Change the file permissions if allowed
     unlink($target_file); //remove the file
 }
 
