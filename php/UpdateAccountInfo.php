a82b9643a78f7bdee15d0b6a251bf6e10dd61cd9
DylanSoares <Dylan.soares.myokanagan.bc.ca>
2021-12-01 16:58:48 -0800

    Updated

--- php/UpdateAccountInfo.php
+++ php/UpdateAccountInfo.php
@@ -94,40 +94,13 @@ if (isset($_POST['team'])) {
 $sql = "UPDATE Drivers SET fname = \"" . $firstname . "\", lname = \"" . $lastname . "\", team =\"" . $teamSelected . "\", car = \"" . $carSelected . "\" WHERE email = \"" . $_SESSION['id'] . "\"";
 $mysqli->query($sql);*/
 
-// Check if image file is a actual image or fake image
 
-/*$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-if ($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-} else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-}
-
-
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    $uploadOk = 0;
-}
-//check if file exists
+/*//check if file exists
 if (file_exists($target_file)) {
     chmod($target_file, 0755); //Change the file permissions if allowed
     unlink($target_file); //remove the file
-}
-
-// Allow certain file formats
-if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif") {
-    $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-// if everything is ok, try to upload file
-} else {
-    move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
 }*/
+
 if (isset($_POST["submitButton"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if ($check !== false) {
